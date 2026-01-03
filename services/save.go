f579085a27be47e926caea7ae92094b03580bb25
DumbCaveSpider <56347227+DumbCaveSpider@users.noreply.github.com>
2026-01-03 08:36:02 +0800

    save improvement ig

--- services/save.go
+++ services/save.go
@@ -337,6 +337,8 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	log.Done("Saved account: %s", req.AccountId)
+
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write([]byte("1"))
